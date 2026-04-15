9eb04f8fc3a421999f4ac9aa0858ca3128139616
Sandadinu Bimsara <127046415+Sandadinu@users.noreply.github.com>
2026-04-16 01:21:17 +0530

    Fix CSP to allow CSS loading

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -2,6 +2,7 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
+    <meta http-equiv="Content-Security-Policy" content="default-src * 'self' data: blob: 'unsafe-inline' 'unsafe-eval';">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Sri Lankan English Literature Hub</title>
     <link rel="preconnect" href="https://fonts.googleapis.com">
